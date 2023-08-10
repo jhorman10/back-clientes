7490895067b781b3db0a8a79723d8ede064b84b8
Jhorman Orozco <jsojhor@gmail.com>
2023-08-10 16:35:32 -0500

    start template api-rest

---
+++ src/main/java/com/codescripters/clientesapirest/clientesapirest/ClientesApirestApplication.java
@@ -0,0 +1,13 @@
+package com.codescripters.clientesapirest.clientesapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApirestApplication.class, args);
+	}
+
+}
